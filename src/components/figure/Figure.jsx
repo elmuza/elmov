c15a1678f00ecb24bba064a4d6117215825d43a4
elmuza <barri1@mail.ru>
2020-11-23 18:31:23 +0200

    добавлен тег  descriptoin

--- src/components/figure/Figure.jsx
+++ src/components/figure/Figure.jsx
@@ -4,7 +4,7 @@ import styles from './figure.module.scss'
 export const Figure = ({service: {img, alt, figcaption}}) => {
   return (
     <figure className={`figure ${styles.page3Figure}`}>
-      <img src={img} alt={alt} className='img-fluid  mx-auto d-flex align-items-start' />
+      <img src={img} alt={alt} className={`img-fluid mx-auto d-flex align-items-start `} />
       <figcaption className={`${styles.caption} text-center overflow-wrap`}>{figcaption}</figcaption>
     </figure>
   )
