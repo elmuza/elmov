22ca4d28056f4ddf09ed843a33af85c74f10ed31
Satilian <viapont@gmail.com>
2022-09-27 11:02:13 +0300

    addres change

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -8,7 +8,7 @@ export const MyMap = () => {
       <YMaps>
         <Map 
           state={{
-            center: [55.889581, 37.555988],
+            center: [55.857442, 37.573223],
             zoom: 13,
           }}
           className={styles.map}
@@ -16,11 +16,11 @@ export const MyMap = () => {
           <Placemark
             options={{hasHint:true}}
             name='Name'
-            geometry={[55.889581, 37.555988]}
+            geometry={[55.857442, 37.573223]}
             properties={{
-              hintContent: 'г.Москва ул. Дубнинская д. 79 стр. 2.',
+              hintContent: 'г.Москва ул. Дубнинская д. 2А стр.4 бокс 173.',
               balloonContent: 'Тел. +7(905)589-97-27',
-              balloonContentHeader: 'г.Москва ул. Дубнинская д. 79 стр. 2',
+              balloonContentHeader: 'г.Москва ул. Дубнинская д. 2А стр.4 бокс 173',
             }}
             modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
           />
